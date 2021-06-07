8ebb38fc727583a60af554a9e7adb561f88a45ef
edan <molphone0328@gmail.com>
2021-06-07 13:50:24 +0900

    f

--- README.md
+++
@@ -1,3 +0,0 @@
-# stac2021
-
-Developed with Unreal Engine 4
--- Source/stac2021/stac2021.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "stac2021.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, stac2021, "stac2021" );
